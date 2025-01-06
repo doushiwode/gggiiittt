13ad4d9ae3cae3d89b677b077f36b3f4236a03d8
ruixu <1330527177@qq.com>
2025-01-06 22:19:36 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+# This is a simple Python script
+
+# Print a greeting message
+print("Hello, World!")
+
+# Define a function to add two numbers
+def add(a, b):
+    return a + b
+
+# Call the function and print the result
+result = add(3, 5)
+print("The sum of 3 and 5 is:", result)
